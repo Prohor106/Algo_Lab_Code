54c641ac88080a819a2a2264b4dff4e963d1e0f1
Prohor Paul <prohorpaul121@gmail.com>
2023-02-15 10:41:11 +0600

    Update Fractional_Knapsack.cpp

--- Fractional_Knapsack.cpp
+++ Fractional_Knapsack.cpp
@@ -11,32 +11,32 @@ using namespace std;
 #define ll long long
 #define all(v) v.begin(),v.end()
 
-struct item{
+struct item {
     string name;
-    double weight , price;
-    void input(){
+    double weight, price;
+    void input() {
         cin >> name >> weight >> price;
     }
 
     double unit_price;
-    void output(){
+    void output() {
         cout << name << " " << weight << " " << price << " " << unit_price << endl;
     }
 };
 
-void bubble_sort(item arr[], int n){
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-i-1; j++){
+void bubble_sort(item arr[], int n) {
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<n-i-1; j++) {
             if(arr[j].unit_price < arr[j+1].unit_price)
                 swap(arr[j], arr[j+1]);
         }
     }
 }
 
-void selection_sort(item arr[], int n){
-    for(int i=0; i<n-1; i++){
+void selection_sort(item arr[], int n) {
+    for(int i=0; i<n-1; i++) {
         int start = i;
-        for(int j=start+1; j<n; j++){
+        for(int j=start+1; j<n; j++) {
             if(arr[j].unit_price > arr[start].unit_price)
                 start = j;
         }
@@ -44,7 +44,7 @@ void selection_sort(item arr[], int n){
     }
 }
 
-bool cmp(item a, item b){
+bool cmp(item a, item b) {
     return a.unit_price > b.unit_price;
 }
 
@@ -55,6 +55,7 @@ void solve(ll cs) {
     cin >> n >> capacity;
 
     item arr[n];
+    cout << "Enter name , weight and prices of Items : " << endl;
     for(int i=0; i<n; i++) {
         arr[i].input();
         arr[i].unit_price = arr[i].price / arr[i].weight;
@@ -63,14 +64,14 @@ void solve(ll cs) {
     //যেকোনো একটা দিয়ে করলেই হবে।
     // sort(arr, arr+n, cmp);
     // bubble_sort(arr, n);
-    selection_sort(arr, n);  
+    selection_sort(arr, n);
 
     for(int i=0; i<n; i++) arr[i].output();
 
     double profit = 0, max_profit = 0;
     vector<string>selected_items;
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++) {
         if(capacity <= 0) break;
 
         double temp_capacity = min(capacity, arr[i].weight);
@@ -87,15 +88,15 @@ void solve(ll cs) {
 
     cout << "Selected Items are \n";
     for(auto u:selected_items) cout << u << " ";
-        cout << endl;
+    cout << endl;
 
     cout << "Max profit from any selected item is " << max_profit << endl;
 }
 
 int main() {
 
-    ios_base::sync_with_stdio(false);
-    cin.tie(0), cout.tie(0);
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(0), cout.tie(0);
 
     ll t=1, cs=1;
     cout << "Enter Testcase : \n";
